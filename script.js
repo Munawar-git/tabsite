600d3b4a6dafdc9ccc3cd00e8b0219a86b1939c6
Munawar <munawerali1993@gmail.com>
2025-02-21 11:34:13 +0100

    Added three new chapters

--- script.js
+++ script.js
@@ -52,25 +52,32 @@ function loadHTML(file, targetElement) {
 document.addEventListener('DOMContentLoaded', function () {
     loadHTML("about_page.htm", "content_area");
 });
+
 // Loading HTML on click on topic
 document.getElementById("tab-icon").addEventListener("click", function () {
     loadHTML("about_page.htm", "content_area");
 });
+
 document.getElementById("00").addEventListener("click", function () {
     loadHTML("00_intro_to_yocto.htm", "content_area");
 });
+
 document.getElementById("01").addEventListener("click", function () {
     loadHTML("01_flashing_image.htm", "content_area");
 });
+
 document.getElementById("02").addEventListener("click", function () {
     loadHTML("02_local_conf.htm", "content_area");
 });
+
 document.getElementById("03").addEventListener("click", function () {
     loadHTML("03_bblayers_conf.htm", "content_area");
 });
+
 document.getElementById("04").addEventListener("click", function () {
     loadHTML("04_add_package.htm", "content_area");
 });
+
 document.getElementById("05").addEventListener("click", function () {
     loadHTML("05_create_add_layer.htm", "content_area");
 });
@@ -81,4 +88,16 @@ document.getElementById("06").addEventListener("click", function () {
 
 document.getElementById("07").addEventListener("click", function () {
     loadHTML("07_var_assign_operator.htm", "content_area");
+});
+
+document.getElementById("08").addEventListener("click", function () {
+    loadHTML("08_helloworld_recipe.htm", "content_area");
+});
+
+document.getElementById("09").addEventListener("click", function () {
+    loadHTML("09_build_tasks.htm", "content_area");
+});
+
+document.getElementById("10").addEventListener("click", function () {
+    loadHTML("10_create_patch.htm", "content_area");
 });
